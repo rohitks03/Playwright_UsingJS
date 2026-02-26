09e59e469a30e82879c4e1a072b6a67d38aaac8b
rkshirsa <rohit.kshirsagar@ansys.com>
2026-02-26 19:18:42 +0530

    Initial commit: Add Playwright JavaScript project files

---
+++ IF_ELSEIF_.js
@@ -0,0 +1,9 @@
+let age = 25;
+
+if (age < 25) {
+    console.log("You can goto goa and drink also.");
+}else if(age >= 18 && age <= 25){
+    console.log("You can goto goa but you can't drink.");
+}else{
+    console.log("Goto home and watch pogo   .");
+}
